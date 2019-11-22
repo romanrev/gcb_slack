2438a70b466cf9ea7620e9ae2799aa8c6c9c9dee
Roman Revyakin <revrom+github@gmail.com>
2019-11-23 09:46:13 +1100

    Real update to Nodejs10

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
-const IncomingWebhook = require('@slack/client').IncomingWebhook;
-const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK
+const { IncomingWebhook } = require('@slack/webhook');
+const url = process.env.SLACK_WEBHOOK_URL;
 
-const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+const webhook = new IncomingWebhook(url);
 
-// subscribe is the main function called by Cloud Functions.
-module.exports.subscribe = (event, callback) => {
- const build = eventToBuild(event.data.data);
+// subscribeSlack is the main function called by Cloud Functions.
+module.exports.subscribeSlack = (pubSubEvent, context) => {
+  const build = eventToBuild(pubSubEvent.data);
 
-// Skip if the current status is not in the status list.
-// Add additional statues to list if you'd like:
-// QUEUED, WORKING, SUCCESS, FAILURE,
-// INTERNAL_ERROR, TIMEOUT, CANCELLED
-  const status = ['QUEUED', 'WORKING', 'SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
+  // Skip if the current status is not in the status list.
+  // Add additional statuses to list if you'd like:
+  // QUEUED, WORKING, SUCCESS, FAILURE,
+  // INTERNAL_ERROR, TIMEOUT, CANCELLED
+  const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
   if (status.indexOf(build.status) === -1) {
-    return callback();
+    return;
   }
 
   // Send message to Slack.
   const message = createSlackMessage(build);
-  webhook.send(message, callback);
+  webhook.send(message);
 };
 
 // eventToBuild transforms pubsub event message to a build object.
 const eventToBuild = (data) => {
-  return JSON.parse(new Buffer(data, 'base64').toString());
+  return JSON.parse(Buffer.from(data, 'base64').toString());
 }
 
-// createSlackMessage create a message from a build object.
+// createSlackMessage creates a message from a build object.
 const createSlackMessage = (build) => {
-  var build_id = build.projectId
-  if (build.source.hasOwnProperty('repoSource')) {
-      build_id = build_id + ': ' + build.source.repoSource.repoName + ' (' +
-          build.source.repoSource.branchName + ')'
-  } else if (build.source.hasOwnProperty('storageSource')) {
-      build_id = build_id + ': ' + build.source.storageSource.bucket + '/' +
-          build.source.storageSource.object
-  }
-  var build_status = build.status
-  var time_taken = -1
-  if (build.status !== 'QUEUED' && build.status !== 'WORKING') {
-      time_taken = ( new Date(build.finishTime) - new Date(build.startTime) )/1000/60
-  }
-  if (build.status === 'FAILURE') {
-      build_status = `\`${build_status}\``
-  }
-  let message = {
-   text: `Build ${build_id}`,
+  const message = {
+    text: `Build \`${build.id}\``,
     mrkdwn: true,
     attachments: [
       {
@@ -53,18 +37,10 @@ const createSlackMessage = (build) => {
         title_link: build.logUrl,
         fields: [{
           title: 'Status',
-          value: build_status
+          value: build.status
         }]
       }
     ]
   };
-  if ( time_taken > 0 ) {
-      message.attachments[0].fields.push(
-          {
-            title: 'Time taken',
-            value: time_taken
-          }
-      )
-  }
-  return message
+  return message;
 }
